5434b65f8ae1c7593c2f021e2eb8dbd43adde983
traalgor <dan@danwilliams.id.au>
2013-08-20 09:21:47 +1000

    Update disc.py

--- disc.py
+++ disc.py
@@ -18,9 +18,22 @@ for line in iter(v_ipcalc.stdout.readline,''):
 
 # summarising available hosts
 print "Total Hosts on subnet: " + v_hosts.rsplit(':',1)[1].rsplit(' ')[1]
-# likely gw's
+# likely gw's, check first three and last three IP addresses in block
+
+## check if first three and last three are valid IP addresses
+
+## if valid, attempt to route traffic manually via them (ttl = 0, get ICMP ttl expired in transit?)
 
 
+# identify common vsrp/hsrp mac addresses (cisco hsrp groups, etc)
 
+
+# identify live hosts on local layer 2
+
+
+# identify duplicate hosts (same mac)
+
+
+# identify duplicate hosts (iterative mac)
 
 
